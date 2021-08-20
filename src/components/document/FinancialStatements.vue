8a5fc7c9005fe4505a63c6d8598c0c295dd7b583
gyorgy.varga <gyorgy.varga@icellmobilsoft.hu>
2021-08-20 17:33:03 +0200

    2019-es beszámoló felvétel

--- src/components/document/FinancialStatements.vue
+++ src/components/document/FinancialStatements.vue
@@ -13,6 +13,16 @@
         >
       </a>
     </div>
+    <div class="w-full max-w-xl p-5 bg-gray-600 text-gray-300 text-center rounded-lg">
+      <div>{{ $t('documents.financialStatements.year.2019') }}</div>
+      <a href="https://res.cloudinary.com/gyurmatag/image/upload/v1629473473/A%20J%C3%B6v%C5%91%20a%20Gyermekek%C3%A9/besza%CC%81molo%CC%81_2019..pdf"
+         target="_blank">
+        <img alt="PDF ikon"
+             src="../../assets/svgs/pdf.svg"
+             class="m-auto h-14 p-2"
+        >
+      </a>
+    </div>
   </div>
 </template>
 
