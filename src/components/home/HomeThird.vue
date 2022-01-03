9bdd4def7343cf44343883453657998268e804f4
gyorgy.varga <gyurmatag@gmail.com>
2022-01-03 19:40:18 +0100

    adatok fejlesztesek

--- src/components/home/HomeThird.vue
+++ src/components/home/HomeThird.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="flex flex-col md:flex-row max-w-6xl py-6 space-y-12 md:space-x-80 md:space-y-0
+  <div class="flex flex-col md:flex-row max-w-6xl py-6 space-y-12 md:space-x-44 md:space-y-0
               my-8 px-4 justify-center items-center bg-white text-center">
     <div class="flex flex-col py-6 text-center md:text-left
                 border-t-2 border-b-2 border-mainbackground">
-      <div class="text-4xl font-bold">8M HUF</div>
-      {{ $t('homeThird.investmentSum') }}
+      <div class="text-4xl font-bold">6,7 m HUF</div>
+      {{ $t('homeThird.incomeSum') }}
     </div>
     <div class="flex flex-col py-6 text-center md:text-left
                 border-t-2 border-b-2 border-mainbackground">
-      <div class="text-4xl font-bold">3M HUF</div>
-      {{ $t('homeThird.annualBudget') }}
+      <div class="text-4xl font-bold">5,6 m HUF</div>
+      {{ $t('homeThird.expenditures') }}
     </div>
     <div class="flex flex-col py-6 text-center md:text-left
                 border-t-2 border-b-2 border-mainbackground">
-      <div class="text-4xl font-bold">5</div>
-      {{ $t('homeThird.allProjectCount') }}
+      <div class="text-4xl font-bold">11 db</div>
+      {{ $t('homeThird.courseCount') }}
     </div>
   </div>
 </template>
